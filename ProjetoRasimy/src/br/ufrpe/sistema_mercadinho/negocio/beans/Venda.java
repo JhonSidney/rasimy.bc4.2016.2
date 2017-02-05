de42716546bdb08a8320609e3e4cd2f750ce5adc
JhonSidney <jhon.sidney@gmail.com>
2017-02-05 15:11:40 -0300

    Alteração na classe Venda e Criação de interface IVenda

--- ProjetoRasimy/src/br/ufrpe/sistema_mercadinho/negocio/beans/Venda.java
+++ ProjetoRasimy/src/br/ufrpe/sistema_mercadinho/negocio/beans/Venda.java
@@ -3,77 +3,95 @@ package br.ufrpe.sistema_mercadinho.negocio.beans;
 public class Venda {
 	private String codigoVenda;
 	private Funcionario funcionario;
-	private Item item;
-	private double valorRecebido;
+	private String dataVenda;
 	private double valorCompra;
+	private double valorRecebido;
 	private double desconto;
 	private double troco;
-	private double dataVenda;
+	private Item item;
+	
 	
 	//construtor
-	public Venda(String codigoVenda, Funcionario funcionario, Item item,
-			double valorRecebido, double valorCompra, double desconto,
-			double troco, double dataVenda) {
+	public Venda(String codigoVenda, Funcionario funcionario,String dataVenda
+			,double valorCompra, double valorRecebido, double desconto,
+			double troco, Item item) {
 		
 		this.codigoVenda = codigoVenda;
 		this.funcionario = funcionario;
-		this.item = item;
-		this.valorRecebido = valorRecebido;
+		this.dataVenda = dataVenda;
 		this.valorCompra = valorCompra;
+		this.valorRecebido = valorRecebido;
 		this.desconto = desconto;
 		this.troco = troco;
-		this.dataVenda = dataVenda;
+		this.item = item;
 	}
 
 
 	public String getCodigoVenda() {
 		return codigoVenda;
 	}
+	
 	public void setCodigoVenda(String codigoVenda) {
 		this.codigoVenda = codigoVenda;
 	}
+	
 	public Funcionario getFuncionario() {
 		return funcionario;
 	}
+	
 	public void setFuncionario(Funcionario funcionario) {
 		this.funcionario = funcionario;
 	}
-	public Item getItem() {
-		return item;
+	
+	public String getDataVenda() {
+		return dataVenda;
 	}
-	public void setItem(Item item) {
-		this.item = item;
+	
+	public void setDataVenda(String dataVenda) {
+		this.dataVenda = dataVenda;
 	}
-	public double getValorRecebido() {
-		return valorRecebido;
-	}
-	public void setValorRecebido(double valorRecebido) {
-		this.valorRecebido = valorRecebido;
-	}
+
 	public double getValorCompra() {
 		return valorCompra;
 	}
+	
 	public void setValorCompra(double valorCompra) {
 		this.valorCompra = valorCompra;
 	}
+	
+	public double getValorRecebido() {
+		return valorRecebido;
+	}
+	
+	public void setValorRecebido(double valorRecebido) {
+		this.valorRecebido = valorRecebido;
+	}
+
+	
 	public double getDesconto() {
 		return desconto;
 	}
+	
 	public void setDesconto(double desconto) {
 		this.desconto = desconto;
 	}
+	
 	public double getTroco() {
 		return troco;
 	}
+	
 	public void setTroco(double troco) {
 		this.troco = troco;
 	}
-	public double getDataVenda() {
-		return dataVenda;
+	
+	public Item getItem() {
+		return item;
 	}
-	public void setDataVenda(double dataVenda) {
-		this.dataVenda = dataVenda;
+	
+	public void setItem(Item item) {
+		this.item = item;
 	}
+	
 	
 	
 	public boolean equals(Venda cupomFiscal){
