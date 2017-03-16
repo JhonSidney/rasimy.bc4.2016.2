238724b844e944ef877aa304ba014df04f739ea2
JhonSidney <jhon.sidney@gmail.com>
2017-03-16 17:22:54 -0300

    commit teste

--- sistema_mercadinho/exceptions/ErroDeNegocioException.java
+++
@@ -1,9 +0,0 @@
-package br.ufrpe.sistema_mercadinho.exceptions;
-
-public class ErroDeNegocioException extends Exception {	
-	
-	public ErroDeNegocioException(String message){
-		super (message);
-	}
-
-}
